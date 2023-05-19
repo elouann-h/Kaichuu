8f80c3c54ef7580deac29f582c3854bacd7768b6
PxndxDev <elouannhosta01@gmail.com>
2023-05-19 12:53:13 +0200

    Progressed

--- app/src/main/java/com/elouannh/kaichuu/MainActivity.kt
+++ app/src/main/java/com/elouannh/kaichuu/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.elouannh.kaichuu
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottleToDisplay: Int = (1..3).random()
+
     }
 }
